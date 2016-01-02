fbb687549b85afe0db8cd79029206432afa6ea67
David Liu <davidbliu@gmail.com>
2016-01-02 13:46:54 -0800

    change shared

--- shared.js
+++ shared.js
@@ -1,5 +1,6 @@
-var ROOT_URL = 'http://localhost/copilot';
+//var ROOT_URL = 'http://localhost/copilot';
 //var ROOT_URL = 'http://wd.berkeley-pbl.com/david/real.html';
+var ROOT_URL = 'http://wd.berkeley-pbl.com/dodo/';
 
 function isValidMapiRequest(request){
   return true;
